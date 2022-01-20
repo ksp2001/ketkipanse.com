b976a7727d9bfddb65ddffa8d883d5da690c3ff2
Ketki Panse <ketki.skpanse@gmail.com>
2022-01-20 14:07:31 -0500

    role change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
           <div className="title">
             <b>👋 I'm <span className="text">Ketki.</span></b>
           </div>
-          <div className="subtitle">Backend Intern <a href="https://www.shopify.ca/" className="link"><b>@Shopify</b></a>. Developer. Language Enthusiast.</div>
+          <div className="subtitle">Mobile Intern <a href="https://www.shopify.ca/" className="link"><b>@Shopify</b></a>. Developer. Language Enthusiast.</div>
           <div className="links"><a href="https://github.com/ksp2001"><i class="fa fa-github"></i></a><a href="https://www.linkedin.com/in/ketki-panse/"><i class="fa fa-linkedin"></i></a></div>
         </div>
       </header>
